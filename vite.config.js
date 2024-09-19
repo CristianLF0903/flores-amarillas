4882ad5c49544b0ee098ec68dfc9f0c79f885440
Cristian Londoño Franco <69688343+CristianLF0903@users.noreply.github.com>
2024-09-18 19:11:29 -0500

    Update vite.config.js base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Especifica la carpeta base del proyecto
-  base: "/CongresoMaternidad/",
+  base: "/flores-amarillas/",
   // Especifica el directorio donde se generará el código compilado
   build: {
     outDir: "dist",
