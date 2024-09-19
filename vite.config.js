fa626a07808370ae63d36c37a98ac94f06663cc2
Cristian Londoño Franco <69688343+CristianLF0903@users.noreply.github.com>
2024-09-18 19:10:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Especifica la carpeta base del proyecto
+  base: "/CongresoMaternidad/",
+  // Especifica el directorio donde se generará el código compilado
+  build: {
+    outDir: "dist",
+  },
+  // Configura los plugins de Vite
   plugins: [react()],
-  base: "/flores-amarillas/",
 });
